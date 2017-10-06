de6bcab3a7ff4df42d453755f8aff233df9a5ce4
ponychan <rzsckbmx@163.com>
2017-10-06 13:48:52 +0800

    3.17.cpp
    调用系统函数

--- ConsoleApplication3_17/ConsoleApplication317.cpp
+++ ConsoleApplication3_17/ConsoleApplication317.cpp
@@ -1,11 +1,23 @@
 // ConsoleApplication317.cpp: 定义控制台应用程序的入口点。
-//
+//3.17.cpp调用系统函数
 
 #include "stdafx.h"
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+const double pi = 3.14159265358979;
 
 int main()
 {
-    return 0;
+	double angle;
+	cout << "Please enter an angle: ";
+	cin >> angle;
+
+	double r = angle*pi / 180;
+	cout << "sin(" << angle << ")=" << sin(r) << endl;
+	cout << "cos(" << angle << ")=" << cos(r) << endl;
+	cout << "tan(" << angle << ")=" << tan(r) << endl;
+	return 0;
 }
 
